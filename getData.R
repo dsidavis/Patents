cd24e742eaf95c222d9d85af3cc20553251b1268
Duncan Temple Lang <duncan@r-project.org>
2016-09-03 17:27:40 -0700

    necessary funs

--- getData.R
+++ getData.R
@@ -1,3 +1,8 @@
+# A script to retrieve all of the links for the  Full text and TIFF links
+# This a two step operation.
+# The front page lists a collection of pages for these.
+# Each of these pages gives a list of the actual files.
+
 library(RCurl)
 library(XML)
 source("funs.R")
@@ -14,8 +19,17 @@ rb = grep("grant/redbook/[0-9]{4}$", ll, value = TRUE)
 
 tars = lapply(rb, function(x) try(getTars(x)))
 
+n = sapply(tars, length)
+sum(n)
+
+
+if(FALSE) {
+# Sample by extension to download and take a look at one for each extension.
 byExt = split(unlist(tars), gsub(".*\\.", "", unlist(tars)))
 eg = sapply(byExt, sample, 1)
 
 sapply(sprintf("wget %s", eg), system) 
+}
+
+
 
