2f837f57eae69bafd3580463e75dc457bc838a2c
Duncan Temple Lang <duncan@r-project.org>
2016-09-03 17:04:53 -0700

    corrected on poisson

--- getData.R
+++ getData.R
@@ -1,4 +1,7 @@
 library(RCurl)
+library(XML)
+source("funs.R")
+
 top = "https://bulkdata.uspto.gov/"
 
 txt = getURLContent(top)
@@ -8,5 +11,11 @@ ll = getHTMLLinks(doc)
 
 rb = grep("grant/redbook/[0-9]{4}$", ll, value = TRUE)
 
-sapply(sprintf("wget %s", rb), system) 
+
+tars = lapply(rb, function(x) try(getTars(x)))
+
+byExt = split(unlist(tars), gsub(".*\\.", "", unlist(tars)))
+eg = sapply(byExt, sample, 1)
+
+sapply(sprintf("wget %s", eg), system) 
 
